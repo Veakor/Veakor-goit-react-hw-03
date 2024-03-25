fe7f4ec021e4e9834cadabd255e2eeb1884d6c91
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-25 19:09:01 +0100

    add

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,25 +2,25 @@
 
 import styles from './Contact.module.css'; 
 
-const Contact = ({ contact, handleDelete }) => {
-    const { id, name, number } = contact;
-  
-    const onDelete = () => {
-      handleDelete(id);
-    
-    };
-  
-    return (
-        <li className={styles.contact}>
-          <div className={styles.contactDetails}>
-            <p className={styles.contactName}>Name: {name}</p>
-            <p className={styles.contactNumber}>Number: {number}</p>
-          </div>
-          <button className={styles.contactButton} onClick={onDelete}>Delete</button>
-        </li>
-      );
-    };
-  export default Contact;
+const Contact = ({ contact, onDeleteContact }) => {
+  const { id, name, number } = contact;
+
+  const handleDelete = () => {
+    onDeleteContact(id);
+  };
+
+  return (
+      <li className={styles.contact}>
+        <div className={styles.contactDetails}>
+          <p className={styles.contactName}>Name: {name}</p>
+          <p className={styles.contactNumber}>Number: {number}</p>
+        </div>
+        <button className={styles.contactButton} onClick={handleDelete}>Delete</button>
+      </li>
+  );
+};
+
+export default Contact;
 
 
 
