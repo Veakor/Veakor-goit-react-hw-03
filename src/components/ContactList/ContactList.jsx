2dd82ffcbdc907dd2223ad59f45557b4684f4ad1
Veakor <135655247+Veakor@users.noreply.github.com>
2024-03-25 16:07:31 +0100

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,7 +3,6 @@ import Contact from '../Contact/Contact';
 const ContactList = ({ contacts }) => {
     return (
       <div>
-        <h2>Contact List</h2>
         <ul>
           {contacts.map(contact => (
             <Contact key={contact.id} contact={contact} />
